9f31676ec57b2b20f01513a12712b1f632a2f7e4
Chirag-55 <123006030+Chirag-55@users.noreply.github.com>
2023-12-31 21:43:19 +0530

    pythonsBasics.py

---
+++ Project1/pythonsBasics.py
@@ -0,0 +1,55 @@
+# 1   Program to print your name
+print("Jala Academy")
+
+#  2  Single-line comment
+print("This is a single-line comment")
+
+'''
+Multi-line comment
+This is a multi-line comment.
+It can span multiple lines.
+'''
+print("This is after the multi-line comment")
+
+# 3  Define variables for different data types
+integer_variable = 10
+boolean_variable = True
+char_variable = 'A'
+float_variable = 3.14
+double_variable = 3.141592653589793
+
+print("Integer Variable:", integer_variable)
+print("Boolean Variable:", boolean_variable)
+print("Char Variable:", char_variable)
+print("Float Variable:", float_variable)
+print("Double Variable:", double_variable)
+
+# 4 Define Local and Global Variables with the Same Name and Print Both Variables to Understand the Scope:
+
+# Global variable
+global_variable = "I am global"
+
+def scope_example():
+    # Local variable with the same name as the global variable
+    global_variable = "I am local"
+    print("Inside the function - Local Variable:", global_variable)
+scope_example()
+
+# Print global variable outside the function
+print("Outside the function - Global Variable:", global_variable)
+
+
+#Output
+'''
+Jala Academy
+This is a single-line comment
+This is after the multi-line comment
+Integer Variable: 10
+Boolean Variable: True
+Char Variable: A
+Float Variable: 3.14
+Double Variable: 3.141592653589793
+Inside the function - Local Variable: I am local
+Outside the function - Global Variable: I am global
+'''
+
