818cb9abb6b71c52bb2fb80e56015238a782f2ff
Chirag-55 <123006030+Chirag-55@users.noreply.github.com>
2024-01-08 01:46:22 +0530

    Update XPath.java

--- Project4/Operation on Web elements/XPath.java
+++ Project4/Operation on Web elements/XPath.java
@@ -11,51 +11,56 @@ public class XPathExamples {
         // Initialize the Firefox driver
         WebDriver driver = new FirefoxDriver();
 
-        // Open the webpage for XPath examples
+        // Open the Magnus application site
         driver.get("http://magnus.jalatechnologies.com/");
 
+        // Perform login
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.id("loginBtn"));
+
+        usernameField.sendKeys("training@jalaacademy.com");
+        passwordField.sendKeys("jobprogram");
+        loginButton.click();
+
+        // Example XPath expressions:
+
         // 1. Read label text and its color
-        readLabelTextAndColor(driver);
+        WebElement labelElement = driver.findElement(By.xpath("//label[@for='username']"));
+        String labelText = labelElement.getText();
+        String labelColor = labelElement.getCssValue("color");
+        System.out.println("Label Text: " + labelText);
+        System.out.println("Label Color: " + labelColor);
 
         // 2. Write an XPath for id, name, className
-        WebElement elementById = driver.findElement(By.xpath("//*[@id='elementId']"));
-        WebElement elementByName = driver.findElement(By.xpath("//*[@name='elementName']"));
-        WebElement elementByClassName = driver.findElement(By.xpath("//*[@class='elementClassName']"));
+        WebElement elementById = driver.findElement(By.xpath("//*[@id='exampleId']"));
+        WebElement elementByName = driver.findElement(By.xpath("//*[@name='exampleName']"));
+        WebElement elementByClassName = driver.findElement(By.xpath("//*[@class='exampleClassName']"));
 
         // 3. How to find an element that contains specific text using contains()
-        WebElement elementWithText = driver.findElement(By.xpath("//*[contains(text(), 'Partial Text')]"));
+        WebElement containsTextElement = driver.findElement(By.xpath("//*[contains(text(), 'Partial Text')]"));
 
         // 4. How to find an element using text()
-        WebElement elementWithExactText = driver.findElement(By.xpath("//*[text()='Exact Text']"));
+        WebElement elementWithText = driver.findElement(By.xpath("//button[text()='Click Me']"));
 
         // 5. How to find an element that starts-with()
-        WebElement elementStartsWith = driver.findElement(By.xpath("//*[starts-with(@id, 'start')]"));
+        WebElement startsWithElement = driver.findElement(By.xpath("//*[starts-with(@id, 'start')]"));
 
         // 6. XPath to select following-sibling and preceding
-        WebElement followingSibling = driver.findElement(By.xpath("//*[@id='currentElement']/following-sibling::div"));
+        WebElement followingSiblingElement = driver.findElement(By.xpath("//*[@id='currentElement']/following-sibling::div"));
         WebElement precedingElement = driver.findElement(By.xpath("//*[@id='currentElement']/preceding::div"));
 
         // 7. XPath to select an element using ancestor, child, parent, and descendant
         WebElement ancestorElement = driver.findElement(By.xpath("//*[@id='currentElement']/ancestor::div"));
-        WebElement childElement = driver.findElement(By.xpath("//*[@id='currentElement']/child::span"));
+        WebElement childElement = driver.findElement(By.xpath("//*[@id='currentElement']/child::div"));
         WebElement parentElement = driver.findElement(By.xpath("//*[@id='currentElement']/parent::div"));
-        WebElement descendantElement = driver.findElement(By.xpath("//*[@id='currentElement']/descendant::span"));
+        WebElement descendantElement = driver.findElement(By.xpath("//*[@id='currentElement']/descendant::div"));
 
         // 8. XPath using OR and AND
-        WebElement elementWithOrCondition = driver.findElement(By.xpath("//*[@id='element1' or @id='element2']"));
-        WebElement elementWithAndCondition = driver.findElement(By.xpath("//*[@id='element1' and @class='elementClass']"));
+        WebElement orElement = driver.findElement(By.xpath("//button[@id='button1' or @id='button2']"));
+        WebElement andElement = driver.findElement(By.xpath("//input[@type='text' and @name='username']"));
 
         // Close the browser
         driver.quit();
-    }
-
-    private static void readLabelTextAndColor(WebDriver driver) {
-        // Read label text and its color
-        WebElement labelElement = driver.findElement(By.xpath("//*[@id='labelId']"));
-        String labelText = labelElement.getText();
-        String labelColor = labelElement.getCssValue("color");
-
-        System.out.println("Label Text: " + labelText);
-        System.out.println("Label Color: " + labelColor);
     }
 }
