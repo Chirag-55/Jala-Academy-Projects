02539b904c9449afdb6f5b51a868ef1d328c29cc
Chirag-55 <123006030+Chirag-55@users.noreply.github.com>
2024-01-08 00:13:51 +0530

    Tables.sql

---
+++ Project2/Tables.sql
@@ -0,0 +1,62 @@
+-- Table1 - SALESPEOPLE
+
+CREATE TABLE SALESPEOPLE (
+    SNUM INT,
+    SNAME VARCHAR(50),
+    city VARCHAR(50),
+    comm DECIMAL(4,2)
+);
+
+INSERT INTOSALESPEOPLE (SNUM, SNAME, city, comm) VALUES
+(1001, 'Peel', 'London', 0.12),
+(1002, 'Serres', 'San Jose', 0.13),
+(1004, 'Motika', 'London', 0.11),
+(1007, 'Rafkin', 'Barcelona', 0.15),
+(1003, 'Axelrod', 'New York', 0.1);
+
+
+--  TABLE - CUST
+
+CREATE TABLE CUST (
+    CNUM INT,
+    CNAME VARCHAR(50),
+    CITY VARCHAR(50),
+    RATING INT,
+    SNUM INT
+);
+
+INSERT INTO cust (CNUM, CNAME, CITY, RATING, SNUM) VALUES
+(2001, 'Hoffman', 'London', 100, 1001),
+(2002, 'Giovanne', 'Rome', 200, 1003),
+(2003, 'Liu', 'San Jose', 300, 1002),
+(2004, 'Grass', 'Brelin', 100, 1002),
+(2006, 'Clemens', 'London', 300, 1007),
+(2007, 'Pereia', 'Rome', 100, 1004);
+
+
+-- TABLE - ORDERS
+
+CREATE TABLE ORDERS (
+    ONUM INT,
+    AMT DECIMAL(10, 2),
+    ODATE DATE,
+    CNUM INT,
+    SNUM INT
+);
+
+INSERT INTO ORDERS (ONUM, AMT, ODATE, CNUM, SNUM) VALUES
+(3001, 18.69, '1994-10-03', 2008, 1007),
+(3003, 767.19, '1994-10-03', 2001, 1001),
+(3002, 1900.10, '1994-10-03', 2007, 1004),
+(3005, 5160.45, '1994-10-03', 2003, 1002),
+(3006, 1098.16, '1994-10-04', 2008, 1007),
+(3009, 1713.23, '1994-10-04', 2002, 1003),
+(3007, 75.75, '1994-10-05', 2006, 1002),
+(3008, 4723.00, '1994-10-05', 2006, 1001),
+(3010, 1309.95, '1994-10-06', 2004, 1002),
+(3011, 9891.88, '1994-10-06', 2006, 1001);
+
+
+
+
+
