95ba7df447d6e7bfc95212b633d5b050839856a8
Chirag-55 <123006030+Chirag-55@users.noreply.github.com>
2024-01-08 01:51:16 +0530

    Update text-box.java

--- Project4/Operation on Web elements/text-box.java
+++ Project4/Operation on Web elements/text-box.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class TextBoxActionsExample {
+public class TextBoxExamples {
     public static void main(String[] args) {
         // Set the path of the geckodriver executable
         System.setProperty("webdriver.gecko.driver", "path/to/geckodriver");
@@ -11,29 +11,41 @@ public class TextBoxActionsExample {
         // Initialize the Firefox driver
         WebDriver driver = new FirefoxDriver();
 
-        // Open the webpage containing the text box
+        // Open the Magnus application site
         driver.get("http://magnus.jalatechnologies.com/");
 
-        // Locate the text box by its ID
-        WebElement textBox = driver.findElement(By.id("textboxId"));
+        // Perform login
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.id("loginBtn"));
 
-        // 1. Type in a Text Box using Selenium WebDriver
-        textBox.sendKeys("Hello, Selenium!");
+        usernameField.sendKeys("training@jalaacademy.com");
+        passwordField.sendKeys("jobprogram");
+        loginButton.click();
 
-        // 2. Get a Text Box value using Selenium WebDriver
+        // Navigate to the text box page
+        driver.findElement(By.linkText("Text Box")).click();
+
+        // Example Text Box interactions:
+
+        // 1. How to Type in text box using Selenium Web Driver
+        WebElement textBox = driver.findElement(By.id("myTextBox"));
+        textBox.sendKeys("This is a sample text");
+
+        // 2. How to get a Text Box value using Selenium Web Driver
         String textBoxValue = textBox.getAttribute("value");
         System.out.println("Text Box Value: " + textBoxValue);
 
-        // 3. Read the placeholder value of a Text Box using getAttribute() method
+        // 3. How to read the placeholder value of a Text Box using getAttribute() method
         String placeholderValue = textBox.getAttribute("placeholder");
-        System.out.println("Placeholder Value: " + placeholderValue);
+        System.out.println("Text Box Placeholder Value: " + placeholderValue);
 
         // 4. Deleting/clear text from the Text Box
         textBox.clear();
 
         // 5. Check if Text Box is enabled/disabled
         boolean isTextBoxEnabled = textBox.isEnabled();
-        System.out.println("Is Text Box Enabled: " + isTextBoxEnabled);
+        System.out.println("Is Text Box Enabled? " + isTextBoxEnabled);
 
         // Close the browser
         driver.quit();
