4a96b711eacb7d3ce3d675f819c3d6c84289ba34
Chirag-55 <123006030+Chirag-55@users.noreply.github.com>
2024-01-08 01:50:04 +0530

    Update radio-button.java

--- Project4/Operation on Web elements/radio-button.java
+++ Project4/Operation on Web elements/radio-button.java
@@ -2,9 +2,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import java.util.List;
 
-public class RadioButtonCheckBoxExample {
+public class RadioButtonExamples {
     public static void main(String[] args) {
         // Set the path of the geckodriver executable
         System.setProperty("webdriver.gecko.driver", "path/to/geckodriver");
@@ -12,89 +11,50 @@ public class RadioButtonCheckBoxExample {
         // Initialize the Firefox driver
         WebDriver driver = new FirefoxDriver();
 
-        // Open the webpage containing radio buttons and checkboxes
+        // Open the Magnus application site
         driver.get("http://magnus.jalatechnologies.com/");
 
-        // 1. Selecting the Radio Button by Label Text / Value
-        selectRadioButton(driver, "Option 1");
+        // Perform login
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.id("loginBtn"));
 
-        // 2. Find out Number of Elements in a Radio Group
-        int radioGroupSize = getRadioGroupSize(driver);
-        System.out.println("Number of radio buttons in the group: " + radioGroupSize);
+        usernameField.sendKeys("training@jalaacademy.com");
+        passwordField.sendKeys("jobprogram");
+        loginButton.click();
+
+        // Navigate to the radio button page
+        driver.findElement(By.linkText("Radio Button")).click();
+
+        // Example Radio Button interactions:
+
+        // 1. Selecting the Radio Button by Label Text / value
+        WebElement radioButtonByLabelText = driver.findElement(By.xpath("//label[text()='Option 1']/input"));
+        radioButtonByLabelText.click();
+
+        // 2. Find out the number of elements in a radio group
+        int radioGroupSize = driver.findElements(By.name("radioGroup")).size();
+        System.out.println("Number of Radio Buttons in the Group: " + radioGroupSize);
 
         // 3. Find out all radio button values
-        List<String> radioButtonValues = getAllRadioButtonValues(driver);
-        System.out.println("Radio button values: " + radioButtonValues);
+        System.out.println("Values of Radio Buttons in the Group:");
+        for (WebElement radio : driver.findElements(By.name("radioGroup"))) {
+            System.out.println(radio.getAttribute("value"));
+        }
 
         // 4. How to get the selected radio button label text?
-        String selectedRadioButtonText = getSelectedRadioButtonText(driver);
-        System.out.println("Selected radio button label text: " + selectedRadioButtonText);
+        WebElement selectedRadio = driver.findElement(By.xpath("//input[@name='radioGroup' and @checked]/following-sibling::label"));
+        System.out.println("Selected Radio Button Label Text: " + selectedRadio.getText());
 
         // 5. Check if Radio Button is selected?
-        boolean isRadioButtonSelected = isRadioButtonSelected(driver, "Option 1");
-        System.out.println("Is radio button selected? " + isRadioButtonSelected);
+        WebElement radioToCheck = driver.findElement(By.xpath("//input[@name='radioGroup'][@value='Option 2']"));
+        System.out.println("Is Radio Button selected? " + radioToCheck.isSelected());
 
         // 6. Check if Radio Button is enabled or disabled?
-        boolean isRadioButtonEnabled = isRadioButtonEnabled(driver, "Option 1");
-        System.out.println("Is radio button enabled? " + isRadioButtonEnabled);
+        WebElement radioToCheck2 = driver.findElement(By.xpath("//input[@name='radioGroup'][@value='Option 3']"));
+        System.out.println("Is Radio Button enabled? " + radioToCheck2.isEnabled());
 
         // Close the browser
         driver.quit();
-    }
-
-    private static void selectRadioButton(WebDriver driver, String labelText) {
-        // Locate the radio button by its label text
-        WebElement radioButton = driver.findElement(By.xpath("//label[text()='" + labelText + "']/input"));
-
-        // Select the radio button
-        radioButton.click();
-    }
-
-    private static int getRadioGroupSize(WebDriver driver) {
-        // Find all radio buttons in the group
-        List<WebElement> radioButtons = driver.findElements(By.cssSelector("input[type='radio']"));
-
-        // Return the number of radio buttons in the group
-        return radioButtons.size();
-    }
-
-    private static List<String> getAllRadioButtonValues(WebDriver driver) {
-        // Find all radio buttons in the group
-        List<WebElement> radioButtons = driver.findElements(By.cssSelector("input[type='radio']"));
-
-        // Create a list to store radio button values
-        List<String> radioButtonValues = new ArrayList<>();
-
-        // Extract values and add to the list
-        for (WebElement radioButton : radioButtons) {
-            radioButtonValues.add(radioButton.getAttribute("value"));
-        }
-
-        return radioButtonValues;
-    }
-
-    private static String getSelectedRadioButtonText(WebDriver driver) {
-        // Find the selected radio button in the group
-        WebElement selectedRadioButton = driver.findElement(By.cssSelector("input[type='radio']:checked"));
-
-        // Get the label text associated with the selected radio button
-        WebElement labelElement = selectedRadioButton.findElement(By.xpath(".."));
-        return labelElement.getText();
-    }
-
-    private static boolean isRadioButtonSelected(WebDriver driver, String labelText) {
-        // Locate the radio button by its label text
-        WebElement radioButton = driver.findElement(By.xpath("//label[text()='" + labelText + "']/input"));
-
-        // Check if the radio button is selected
-        return radioButton.isSelected();
-    }
-
-    private static boolean isRadioButtonEnabled(WebDriver driver, String labelText) {
-        // Locate the radio button by its label text
-        WebElement radioButton = driver.findElement(By.xpath("//label[text()='" + labelText + "']/input"));
-
-        // Check if the radio button is enabled
-        return radioButton.isEnabled();
     }
 }
